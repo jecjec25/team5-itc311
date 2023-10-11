8179fa5c018c39092ba1a80acfa96cd63087ac25
jecjec25 <130357588+jecjec25@users.noreply.github.com>
2023-10-11 15:19:40 +0800

    con

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Manage from '../views/ManageView.vue'
+import name from '../views/MyName.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'manage',
-    component: Manage
+    name: 'myname',
+    component: name
   },
   {
     path: '/about',
@@ -23,7 +23,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/app.vue')
   },
-  
+  {
+    path: '/Manage',
+    name: 'Manage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Manage.vue')
+  },
+
 ]
 
 const router = createRouter({
---
+++ src/views/Manage.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <p>
+            Members:
+            <br>Christia Dela Chica 
+            <br>jennifer ramirez
+            <br>kyle curba
+        </p>
+    </v-container>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Manage',
+  
+});
+</script>
