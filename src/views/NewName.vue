478a2bce142a692a1eb0d8828e1995ae57f6fc83
Ramirez, Jennifer M <144778693+jen2112@users.noreply.github.com>
2023-10-11 21:30:37 +0800

    main

---
+++ src/views/NewName.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <p>
+            Jennifer M. Ramirez
+        </p>
+    </v-container>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NewName',
+  
+});
+</script>
